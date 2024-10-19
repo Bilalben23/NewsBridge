c9da6f2de79d40160b3566ec442ed502651885db
Bilal Benyoussef <bilalbeny735@gmail.com>
2024-10-19 19:53:48 +0200

    add the router and set up the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './components/Layout'
+import CategoryPage from './pages/CategoryPage'
+import SearchResults from './pages/SearchResults'
 
 export default function App() {
   return (
-    <div>
-      <h1>The app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/categories' element={<CategoryPage />} />
+          <Route path='/search' element={<SearchResults />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
     return (
         <header>
-            The header goes here...
+            <div>
+                <img src='' alt='' />
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/categories">Categories</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/search">Search</NavLink>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
